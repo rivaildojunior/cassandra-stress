b113da7cc71eee4c272a76748c3c02a52bcfc85e
Rivaildo Sousa <rivaildojunior@yahoo.com.br>
2020-06-12 17:25:10 -0300

    filtro por data

--- src/main/java/br/com/freitas/dse/stress/domain/repository/UserCustomRepositoryImpl.java
+++ src/main/java/br/com/freitas/dse/stress/domain/repository/UserCustomRepositoryImpl.java
@@ -2,8 +2,10 @@ package br.com.freitas.dse.stress.domain.repository;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.data.cassandra.core.CassandraOperations;
@@ -16,59 +18,60 @@ import br.com.freitas.dse.stress.domain.model.User;
 
 @Repository
 public class UserCustomRepositoryImpl implements UserCustomRepository {
-    
+
 	private final CassandraOperations cqlTemplate;
 	private String where = "";
-	
-    public UserCustomRepositoryImpl(CassandraOperations cqlTemplate) {
-        this.cqlTemplate = cqlTemplate;
-    }
 
-    public List<User> getQuery(Map<String, Object> filtro, String order, Integer start, Integer size) {
-        Select.Where select = QueryBuilder.select().from("tb_user").where();
-        where = "";
-        
-        filtro.forEach((key, value) -> {
-        	if (value != null) {
-        		where = where + "\"fq\":\""+key+":"+value+ "\"";
-        	}
-//            if (value != null) {
-//                if (key.equals("birthday_ini")) {
-//                    select.and(QueryBuilder.gte("birthday", this.convertToDate(value)));
-//                    return;
-//                }
-//
-//                if (key.equals("birthday_end")) {
-//                    select.and(QueryBuilder.lte("birthday", this.convertToDate(value)));
-//                    return;
-//                }
-//
-//                select.and(QueryBuilder.eq(key, value));
-//            }
-        });
+	public UserCustomRepositoryImpl(CassandraOperations cqlTemplate) {
+		this.cqlTemplate = cqlTemplate;
+	}
 
-        if (order != null) {
-        	if (!where.equals("")) {
-        		select.and(QueryBuilder.eq("solr_query", "{\"q\":\"*:*\", \"start\":\""+start+"\","+where+", \"sort\":\""+order+" asc\"}" ));
-        	} else {
-        		select.and(QueryBuilder.eq("solr_query", "{\"q\":\"*:*\", \"start\":\""+start+"\", \"sort\":\""+order+" asc\"}" ));
-        	}
-        }else {
-        	if (!where.equals("")) {
-            	select.and(QueryBuilder.eq("solr_query", "{\"q\":\"*:*\", \"start\":\""+start+"\","+where+"}" ));
-        	} else {
-            	select.and(QueryBuilder.eq("solr_query", "{\"q\":\"*:*\", \"start\":\""+start+"\"}" ));
-        	}
-        }
-        select.limit(size);
-        return this.cqlTemplate.select(select, User.class);
-    }
+	public List<User> getQuery(Map<String, Object> filtro, String order, Integer start, Integer size) {
+		Select.Where select = QueryBuilder.select().from("tb_user").where();
+		where = "";
 
-    private Date convertToDate(Object Object) {
-        LocalDate date = (LocalDate) Object;
+		filtro.forEach((key, value) -> {
+			if (value != null) {
+				if (key.equals("birthday_ini")) {
+					where = where + "\"fq\":\"birthday:[" +  value.toString() + "T00:00:00Z";
+					// select.and(QueryBuilder.gte("birthday", this.convertToDate(value)));
+					// return;
+				} else
 
-        return Date.from(date.atStartOfDay()
-                .atZone(ZoneId.of("GMT"))
-                .toInstant());
-    }
+				if (key.equals("birthday_end")) {
+					where = where + " TO " + value + "T00:00:00Z]\"";
+					// return;
+				} else {
+					where = where + "\"fq\":\"" + key + ":" + value + "\"";
+				}
+
+				// select.and(QueryBuilder.eq(key, value));
+			}
+			// ""fq":"birthday:[2016-03-15T00:00:00Z TO 2017-02-01T00:00:00Z]"
+		});
+
+		if (order != null) {
+			if (!where.equals("")) {
+				select.and(QueryBuilder.eq("solr_query",
+						"{\"q\":\"*:*\", \"start\":\"" + start + "\"," + where + ", \"sort\":\"" + order + " asc\"}"));
+			} else {
+				select.and(QueryBuilder.eq("solr_query",
+						"{\"q\":\"*:*\", \"start\":\"" + start + "\", \"sort\":\"" + order + " asc\"}"));
+			}
+		} else {
+			if (!where.equals("")) {
+				select.and(QueryBuilder.eq("solr_query", "{\"q\":\"*:*\", \"start\":\"" + start + "\"," + where + "}"));
+			} else {
+				select.and(QueryBuilder.eq("solr_query", "{\"q\":\"*:*\", \"start\":\"" + start + "\"}"));
+			}
+		}
+		select.limit(size);
+		return this.cqlTemplate.select(select, User.class);
+	}
+
+	private Date convertToDate(Object Object) {
+		LocalDate date = (LocalDate) Object;
+
+		return Date.from(date.atStartOfDay().atZone(ZoneId.of("GMT")).toInstant());
+	}
 }
